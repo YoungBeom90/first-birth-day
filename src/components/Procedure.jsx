45a8c8168bf0e7c5b67627d25050193b49e0cd28
woodyenerdot <ybkim@enerdot.co.kr>
2023-10-06 12:28:16 +0900

    식순 테스트 진행

--- src/components/Procedure.jsx
+++ src/components/Procedure.jsx
@@ -18,9 +18,48 @@ export const Procedure = () => {
                     <ColumnMC>진행</ColumnMC>
                 </ContentBox>
                 <ContentBox>
-                    <ContentName>기도 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</ContentName>
-                    <ContentInfo>찬송가 155장</ContentInfo>
-                    <ContentMC>김유택 목사님</ContentMC>
+                    <ContentName>예배 기도</ContentName>
+                    <ContentMC>김유택 목사</ContentMC>
+                </ContentBox>
+                <ContentBox>
+                    <ContentName>찬송</ContentName>
+                    <ContentInfo>565장</ContentInfo>
+                    <ContentMC>다같이</ContentMC>
+                </ContentBox>
+                <Description>
+                    <DescriptionText>1. 예수께로 가면 나는 기뻐요 걱정 근심 없고 정말 즐거워</DescriptionText>
+                    <DescriptionText>(후렴) 예수께로 가면 나는 기뻐요 나와 같은 아이 부르셨어요</DescriptionText>
+                    <DescriptionText>2. 예수께로 가면 맞아주시고 나를 사랑하사 용서하셔요</DescriptionText>
+                    <DescriptionText>3. 예수께로 가면 손을 붙잡고 어디서나 나를 인도하셔요</DescriptionText>
+                </Description>
+                <ContentBox>
+                    <ContentName>성경 봉독</ContentName>
+                    <ContentInfo>시편 1 : 1~3</ContentInfo>
+                    <ContentMC>김유택 목사</ContentMC>
+                </ContentBox>
+                <Description>
+                    <DescriptionText>1 복 있는 사람은 악인들의 꾀를 따르지 아니하며 죄인들의 길에 서지 아니하며 오만한 자들의 자리에 앉지 아니하고</DescriptionText>
+                    <DescriptionText>2 오직 여호와의 율법을 즐거워하여 그의 율법을 주야로 묵상하는 도다</DescriptionText>
+                    <DescriptionText>3 그는 시냇가에 심은 나무가 철을 따라 열매를 맺으며 그 잎사귀가 마르지 아니함 같으니 그가 하는 모든 일이 다 형통하리로다</DescriptionText>
+                </Description>
+                <ContentBox>
+                    <ContentName>설교</ContentName>
+                    <ContentInfo>예수님처럼 자라나는 아이</ContentInfo>
+                    <ContentMC>김유택 목사</ContentMC>
+                </ContentBox>
+                <ContentBox>
+                    <ContentName>축복송</ContentName>
+                    <ContentInfo>너는 시냇가에 심은 나무라</ContentInfo>
+                    <ContentMC>다같이</ContentMC>
+                </ContentBox>
+                <Description>
+                    <DescriptionText>너는 시냇가에 심은 나무라</DescriptionText>
+                    <DescriptionText>하나님의 사랑 안에 믿음 뿌리 내리고</DescriptionText>
+                    <DescriptionText>주의 뜻대로 주의 뜻대로 항상 살리라</DescriptionText>
+                </Description>
+                <ContentBox>
+                    <ContentName>축도</ContentName>
+                    <ContentMC>김유택 목사</ContentMC>
                 </ContentBox>
             </Page>
         </Background>
@@ -28,7 +67,8 @@ export const Procedure = () => {
 }
 
 const Background = styled.div`
-  height: ${window.innerHeight}px;
+  height: auto;
+  min-height: ${window.innerHeight}px;
   margin: 0;
   display: flex;
   justify-content: center;
@@ -54,22 +94,21 @@ const GoHome = styled.div`
 
 const Page = styled.div`
   width: 100%;
-  height: 58%;
-  margin: auto 5%;
-  padding: 30px;
+  height: auto;
+  margin: 28% 5% 10% 5%;
+  padding: 5%;
   border-radius: 10px;
   background-color: whitesmoke;
 `
 
 const TopSection = styled.div`
-  height: auto;
   display: flex;
   justify-content: center;
-  margin-bottom: 20px;
 `
 
-const Title = styled.h2`
-  margin: 5px;
+const Title = styled.text`
+  font-size: 130%;
+  font-weight: bold;
 `
 
 const ContentBox = styled.div `
@@ -77,17 +116,40 @@ const ContentBox = styled.div `
   justify-content: space-between;
 `
 
-const ColumnName = styled.h3`
-    font-weight: bold;
+const Description = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 5px;
 `
 
-const ColumnMC = styled.h3`
-  font-weight: bold;`
+const DescriptionText = styled.text`
+  padding: 0 30px 0 30px;
+  font-size: 80%;
+  font-weight: bold;
+`
+
+const ColumnName = styled.text`
+  font-size: 110%;
+  font-weight: bold;
+  margin: 5% 0;
+`
+
+const ColumnMC = styled.text`
+  font-size: 110%;
+  font-weight: bold;
+  margin: 5% 0;
+`
 
 const ContentName = styled.div`
-  
+  font-size: 100%;
 `
 
-const ContentInfo = styled.div``
+const ContentInfo = styled.div`
+  font-size: 90%;
+`
 
-const ContentMC = styled.div``
+const ContentMC = styled.div`
+  font-size: 100%;
+`
